da4a27d4d38bb6a1885df4ac9237559b94ee536f
Haxosura <123653087+Haxosura@users.noreply.github.com>
2023-11-06 17:58:07 +0000

    Code For Enemy AI Controller
    Added Code to Enemy AI Controller .cpp and .h
    Added Code to Build Project

--- Source/API_Project/EnemyAIController.h
+++ Source/API_Project/EnemyAIController.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/** Custom AI Controller For Enemy*/
 
 #pragma once
 
@@ -6,12 +6,27 @@
 #include "AIController.h"
 #include "EnemyAIController.generated.h"
 
-/**
- * 
- */
+class UBehaviorTreeComponent;
+ 
 UCLASS()
 class API_PROJECT_API AEnemyAIController : public AAIController
 {
 	GENERATED_BODY()
 	
+public:
+	AEnemyAIController();
+
+protected:
+	virtual void BeginPlay();
+	virtual void OnPossess(APawn* InPawn);
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI", meta = (AllowPrivateAccess = true));
+	TObjectPtr<UBehaviorTree> BehaviorTree;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI", meta = (AllowPrivateAccess = true));
+	TObjectPtr<UBehaviorTreeComponent> BehaviorTreeComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI", meta = (AllowPrivateAccess = true));
+	TObjectPtr<UBlackboardComponent> BlackboardComponent;
 };
