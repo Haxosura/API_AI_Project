dffc7dad064492e41d6dfeb0cec74b4861a34693
Haxosura <123653087+Haxosura@users.noreply.github.com>
2023-11-06 18:02:18 +0000

    Re-Added Controller

---
+++ Source/API_Project/EnemyAIController.h
@@ -0,0 +1,32 @@
+/** Custom AI Controller For Enemy*/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "EnemyAIController.generated.h"
+
+class UBehaviorTreeComponent;
+
+UCLASS()
+class API_PROJECT_API AEnemyAIController : public AAIController
+{
+    GENERATED_BODY()
+
+public:
+    AEnemyAIController();
+
+protected:
+    virtual void BeginPlay();
+    virtual void OnPossess(APawn* InPawn);
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI", meta = (AllowPrivateAccess = true));
+    TObjectPtr<UBehaviorTree> BehaviorTree;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI", meta = (AllowPrivateAccess = true));
+    TObjectPtr<UBehaviorTreeComponent> BehaviorTreeComponent;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI", meta = (AllowPrivateAccess = true));
+    TObjectPtr<UBlackboardComponent> BlackboardComponent;
+};
