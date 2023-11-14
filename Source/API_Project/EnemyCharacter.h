d9c483efcc1818b827a0275ac4cf51ad3e6ef09b
Haxosura <123653087+Haxosura@users.noreply.github.com>
2023-11-14 10:34:57 +0000

    PatrolPath
    Added BP_PatrolPath Blueprint
    Added PatrolPath c++ Script
    Added FindTargetLocation c++Script
    Updated EnemyCharacter

--- Source/API_Project/EnemyCharacter.h
+++ Source/API_Project/EnemyCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "PatrolPath.h"
 #include "EnemyCharacter.generated.h"
 
 UCLASS()
@@ -15,15 +16,18 @@ public:
 	// Sets default values for this character's properties
 	AEnemyCharacter();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI", meta = (AllowPrivateAccess = "true"))
+	APatrolPath* PatrolPath;
 
 };
